9968061eb77ff11fb6e4f729440a189e0a5b0ed9
Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
2023-02-28 17:35:56 -0300

    M 03 (#2)
    * CambiosUsersJson
    
    * FixErrores

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ import indexRouter from "./routes/index.js";
 import cors from "cors";
 import axios from "axios";
 import "./config/database.js"; //requiero la configuracion de la db
-
+import { create } from "domain";
+import { errorHandler } from "./middleware/error.js";
 let app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+
+
+//middlewars
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,6 +26,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
+//routes
+
 app.use("/", indexRouter);
+
+function errorNotFound(req, res, next){
+  next(createError(404, 'La ruta no existe'))
+}
+
+app.use(errorNotFound)
+
+app.use(errorHandler)
+
 
 export default app;
---
+++ middleware/error.js
@@ -0,0 +1,11 @@
+export function errorHandler(err, req, res, next) {
+  console.error(err.stack)
+  res.status(err.status || 500).json({
+    status: err.status,
+    message: err.message
+  })
+}
+
+export function errorNotFound(req, res,next) {
+  next(createError(404,'La ruta no existe'))
+}
