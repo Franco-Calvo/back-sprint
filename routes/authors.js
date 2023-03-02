0c49068e3fe2de501d01133a822502e80a7d13cf
DanielOQ <danielorozco0208@gmail.com>
2023-03-02 10:29:08 -0500

    fix

--- routes/authors.js
+++ routes/authors.js
@@ -3,11 +3,11 @@ import controller from "../controllers/authors/create.js";
 import schema from "../schemas/authors.js";
 import validator from '../middleware/validator.js'
 import is_active from "../middleware/authors/is_active.js";
-//import passport from '../middleware/passport.js'
+import passport from '../middleware/passport.js'
 
 const { create } = controller
 let router = express.Router();
 
-router.post("/authors"/*,passport.authenticate('jwt',{session:false})*/,validator(schema),create );
+router.post("/authors",passport.authenticate('jwt',{session:false}),validator(schema),create );
 
 export default router;
