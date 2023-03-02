d2ec6c0bf81e752709a34ab572c4c887ee11e401
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-03-02 12:19:52 -0300

    fixeds

--- models/Chapter.js
+++ models/Chapter.js
@@ -1,23 +1,17 @@
 import mongoose from 'mongoose'
 
 const chapterSchema = new mongoose.Schema({
-    manga_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Manga' },
+    manga_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'mangas' },
     title: { type: String, required: true },
     cover_photo: { type: String, required: true },
     pages: [{ type: String, required: true }],
     order: { type: Number },
-
 },
     {
         time_stamps: { type: Date, default: Date.now, required: true },
     }
-
-
 );
 
-let Chapter = mongoose.model('Chapters', chapterSchema);
+let Chapter = mongoose.model('chapters', chapterSchema);
 
 export default Chapter
-
-
-//CHAPTER
