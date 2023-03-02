2055684545a5da56cff2d9e4f43767bee80d8a83
DanielOQ <danielorozco0208@gmail.com>
2023-03-02 10:34:58 -0500

    is_propertu_of

--- middleware/authors/is_property_of.js
+++ middleware/authors/is_property_of.js
@@ -1,7 +1,9 @@
 import Manga from '../../models/Manga.js'
 
 async function is_property_of(req,res,next) {
-    const manga = await Manga.findOne({author_id: req.body.author_id})
+    const manga = await Manga.findOne({
+        manga_id:req.body.manga_id,
+        author_id: req.body.author_id})
     if (manga) {
         return next()
     }
