9aa6516486e884d0a65ee1a791cafda33fb1eadc
RominaGaite <gaiteromina@gmaul.com>
2023-03-05 14:27:33 -0300

    correciones y mofidifaciones de categorias

--- routes/manga.js
+++ routes/manga.js
@@ -2,16 +2,15 @@ import express from 'express'
 import mangaSchema from '../schemas/mangas.js'
 import validator from '../middleware/validator.js'
 import mangaCreate from '../controllers/manga/create.js'
-import exist_title from '../middleware/manga/exist_title.js'
-import is_active from '../middleware/authors/is_active.js'
+//import exist_title from '../middleware/manga/exist_title.js'
+import allController from '../controllers/category/all.js'
+//import is_active from '../middleware/authors/is_active.js'
 import passport from '../middleware/passport.js'
 
 let router = express.Router()
 const { create } = mangaCreate
-/* router.get('/', function (req, res, next) {
-    res.send('New Manga');
-}); */
+const { all } = allController
 
-router.post("/", passport.authenticate('jwt',{session:false}) ,is_active, validator(mangaSchema),exist_title, create);
-
+router.post("/", passport.authenticate('jwt',{session:false}) , validator(mangaSchema), create);
+router.get('/',all)
 export default router
