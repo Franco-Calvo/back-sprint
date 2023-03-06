6225d6fe80e57bbb9b69e74086aed2fa14b1c9f5
RominaGaite <gaiteromina@gmaul.com>
2023-03-06 12:52:36 -0300

    correciones

--- routes/manga.js
+++ routes/manga.js
@@ -2,15 +2,15 @@ import express from 'express'
 import mangaSchema from '../schemas/mangas.js'
 import validator from '../middleware/validator.js'
 import mangaCreate from '../controllers/manga/create.js'
-//import exist_title from '../middleware/manga/exist_title.js'
+import exist_title from '../middleware/manga/exist_title.js'
 import allController from '../controllers/category/all.js'
-//import is_active from '../middleware/authors/is_active.js'
+import is_active from '../middleware/authors/is_active.js'
 import passport from '../middleware/passport.js'
 
 let router = express.Router()
 const { create } = mangaCreate
 const { all } = allController
 
-router.post("/", passport.authenticate('jwt',{session:false}) , validator(mangaSchema), create);
+router.post("/", passport.authenticate('jwt',{session:false}),is_active , validator(mangaSchema),exist_title, create);
 router.get('/',all)
 export default router
