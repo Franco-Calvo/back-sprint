1237d8702d8a328e414bb4a52be464a40ecd3f6e
RominaGaite <73299120+RominaGaite@users.noreply.github.com>
2023-03-09 10:51:57 -0300

    M-11 controllador chapters (#17)
    * M 03 (#2)
    
    * CambiosUsersJson
    
    * FixErrores
    
    * categories controller
    
    * coorecciones
    
    * correciones y mofidifaciones de categorias
    
    * correciones
    
    * ruta get id
    
    * controllador chapters
    
    ---------
    
    Co-authored-by: Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
    Co-authored-by: RominaGaite <gaiteromina@gmaul.com>

--- routes/chapters.js
+++ routes/chapters.js
@@ -8,16 +8,17 @@ import addFrontPhoto from '../middleware/chapters/add_front_photo.js'
 import existsOrder from '../middleware/chapters/exists_order.js'
 import is_active from '../middleware/authors/is_active.js'
 import is_property_of from '../middleware/authors/is_property_of.js'
-
+import getCharpters from '../controllers/chapters/get_charpters.js'
 
 let router = express.Router()
 
-
 const { create } = chapterCreate
-router.get('/', function (req, res, next) {
+const {get_chapter}=getCharpters
+
+/* router.get('/', function (req, res, next) {
     res.send('chapters here');
-})
+}) */
 
 router.post('/', passport.authenticate('jwt', { session: false }), is_active, validator(postschema), is_property_of, existsOrder, nextOrder, addFrontPhoto, create)
-
+router.get('/',get_chapter)
 export default router
