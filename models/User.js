fa3a612decb360651a72410e99907655ccb91281
Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
2023-03-09 18:22:47 -0300

    M05 middle (#19)
    * Cambios
    
    * Finds_id
    
    * ArregloDeMensajesPersonalizados
    
    * Errores de login solucionados
    
    * Cambios mínimos
    
    * Cambios stage
    
    * Cambios en el back con la nueva data
    
    * Fix de email

--- models/User.js
+++ models/User.js
@@ -1,21 +1,23 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const schema = new mongoose.Schema(
-    {
-        mail: { type: String, require: true },
-        password: { type: String, require: true },
-        photo: { type: String, require: true },
-        is_online: { type: Boolean, require: true },
-        is_admin: { type: Boolean, require: true },
-        is_author: { type: Boolean, require: true },
-        is_company: { type: Boolean, require: true },
-        is_verified: { type: Boolean, require: true },
-        verify_code: { type: String, require: true }
-    },{
-        timestamps: true
-    }
-)
+  {
+    email: { type: String, required: true },
+    name: {type: String, required: true},
+    password: { type: String, required: true },
+    photo: { type: String, required: true },
+    is_online: { type: Boolean, required: true },
+    is_admin: { type: Boolean, required: true },
+    is_author: { type: Boolean, required: true },
+    is_company: { type: Boolean, required: true },
+    is_verified: { type: Boolean, required: true },
+    verify_code: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('users',schema)
+const User = mongoose.model("users", schema);
 
-export default User
+export default User;
