18f88801102499a7dde4591f63e7a1c90ddb150b
RominaGaite <gaiteromina@gmaul.com>
2023-03-13 18:08:05 -0300

    .

--- controllers/manga/get_one.js
+++ controllers/manga/get_one.js
@@ -4,7 +4,9 @@ const controller = {
   get_one: async (req, res) => {
     let one = await manga
       .findOne({ _id: req.params.id })
-      .select("title description -_id");
+      .select("title cover_photo description author_id category -_id")
+      .populate("category_id","name -_id")
+      .populate("author_id", "name -_id")
     if (one) {
       return res.status(200).json({ manga: one });
     } else {
