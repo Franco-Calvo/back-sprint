423b1544f561f2e3d1d90264c3b1a83a390fbed3
Franco Calvo <fcalvo33@gmail.com>
2023-03-13 17:20:34 -0300

    Back M10 Terminado

--- controllers/manga/get_mangas.js
+++ controllers/manga/get_mangas.js
@@ -2,6 +2,7 @@ import Manga from "../../models/Manga.js";
 
 const controller = {
   allget: async (req, res, next) => {
+    
     let consultas = {};
 
     let pagination = {
@@ -12,8 +13,8 @@ const controller = {
     if (req.query.title) {
       consultas.title = new RegExp(req.query.title.trim(), "i");
     }
-    if (req.query.category_id) {
-      consultas.category_id = new RegExp(req.query.category_id.trim(), "i");
+    if (req.query.category) {
+      consultas.category_id = req.query.category.split(",");
     }
     if (req.query.page) {
       pagination.page = req.query.page;
@@ -23,12 +24,12 @@ const controller = {
     }
     try {
       let all = await Manga.find(consultas)
-        .select("title -_id")
+        .select("title category_id cover_photo _id")
         .sort({ title: 1 })
         .skip(
           pagination.page > 0 ? (pagination.page - 1) * pagination.limit : 0
         )
-        .limit(pagination > 0 ? pagination.limit : 0);
+        .limit(pagination.limit > 0 ? pagination.limit : 0);
       return res.status(200).json({ mangas: all });
     } catch (err) {
       next(err);
