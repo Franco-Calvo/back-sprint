28ec085e29ccce7d9ba5fb18020c793b6119d8ae
DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>
2023-03-15 16:05:18 -0500

    M 09 (#26)
    * M 03 (#2)
    
    * CambiosUsersJson
    
    * FixErrores
    
    * correciones schema y routes
    
    * correciones
    
    * actualizacion de router
    
    * correciones del validador
    
    * correciones
    
    * m-09-controller
    
    * correciones
    
    * correciones
    
    * arreglo status
    
    * fix
    
    ---------
    
    Co-authored-by: Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>

--- controllers/authors/get_one.js
+++ controllers/authors/get_one.js
@@ -2,16 +2,23 @@ import Author from "../../models/Author.js";
 
 const controller = {
   get_one: async (req, res) => {
-    let author = await Author.findOne({_id: req.params.id}).select(
-      "name city country createdAt photo -_id"
-    );
-    if (author) {
-      return res.status(200).json({
-        success: true,
-        data: author,
-      });
-    } else {
-      return res.status(400).json({
+    try {
+      let author = await Author.findOne({_id: req.params.id}).select(
+        "name city country createdAt photo -_id"
+      );
+      if (author) {
+        return res.status(200).json({
+          success: true,
+          data: author,
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: "This author dont exist already!",
+        });
+      }
+    } catch (error) {
+      return res.status(404).json({
         success: false,
         message: "This author dont exist already!",
       });
