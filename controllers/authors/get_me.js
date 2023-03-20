2bc388a55335e7f7e7c7a81205b891c2a623b833
RominaGaite <gaiteromina@gmaul.com>
2023-03-20 16:57:15 -0300

    correccion

--- controllers/authors/get_me.js
+++ controllers/authors/get_me.js
@@ -4,8 +4,7 @@ import createError from "http-errors"
 const controller = {
     get_me: async (req, res) => {
         try {
-            const author = req.author;
-            let me = await Author.findOne({ author })
+            let me = await Author.findOne({ user_id:req.user })//aca busca el usuario que esta registrado
                 .select("name last_name city country date photo ")
             if (me) {
                 return res.status(200).json({
