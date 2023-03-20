660d180ac754cad743fe9a57e1a7ac8def4d5109
DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>
2023-03-20 14:56:57 -0500

    M 14 (#28)
    * M 03 (#2)
    
    * CambiosUsersJson
    
    * FixErrores
    
    * correciones schema y routes
    
    * correciones
    
    * actualizacion de router
    
    * correciones del validador
    
    * correciones
    
    * m-09-controller
    
    * correciones
    
    * correciones
    
    * arreglo status
    
    * fix
    
    * fix
    
    * add controlers update and destroy + test t-04
    
    * correciones
    
    * .
    
    * .
    
    * .
    
    * .
    
    ---------
    
    Co-authored-by: Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>

--- controllers/manga/get_one.js
+++ controllers/manga/get_one.js
@@ -3,24 +3,22 @@ import manga from "../../models/Manga.js";
 const controller = {
   get_one: async (req, res) => {
     try {
-      let one = await manga.findOne({ _id: req.params.id })
-          .select("title cover_photo description author_id category _id")
-          .populate("category_id","name -_id")
-          .populate("author_id", "name _id")
-        if (one) {
-          return res.status(200).json({ manga: one });
-        } else {
-          return res.status(400).json({
-            success: false,
-            message: "This manga dont exist already!",
-          });
-        }
-    }
-    catch (error) {
-      console.log(error)
+      let one = await manga
+        .findOne({_id: req.params.id})
+        .select("title cover_photo description author_id category _id")
+        .populate("category_id", "name -_id")
+        .populate("author_id", "name _id");
+      if (one) {
+        return res.status(200).json({manga: one});
+      } else {
+        return res.status(400).json({
+          success: false,
+          message: "This manga dont exist already!",
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   },
-}
+};
 export default controller;
-
-
--- middleware/authors/is_property_of.js
+++ middleware/authors/is_property_of.js
@@ -2,21 +2,20 @@ import Manga from "../../models/Manga.js";
 import Author from "../../models/Author.js";
 
 async function is_property_of(req, res, next) {
+  req.body.manga_id === undefined ? (req.body.manga_id = req.params.id) : null;
   const author = await Author.findOne({
     user_id: req.user._id,
   });
   const manga = await Manga.findOne({
-    _id: req.params.id,
+    _id: req.body.manga_id,
     author_id: author._id,
   });
-  console.log(manga)
   if (manga) {
     return next();
   }
   return res.status(400).json({
     succes: false,
     message: "The manga is not by the author!",
-    data: manga,
   });
 }
 export default is_property_of;
