d1263380ebde8846c3be95272b0b666759587f89
DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>
2023-03-21 16:52:06 -0500

    M 14 (#34)
    * M 03 (#2)
    
    * CambiosUsersJson
    
    * FixErrores
    
    * correciones schema y routes
    
    * correciones
    
    * actualizacion de router
    
    * correciones del validador
    
    * correciones
    
    * m-09-controller
    
    * correciones
    
    * correciones
    
    * arreglo status
    
    * fix
    
    * fix
    
    * add controlers update and destroy + test t-04
    
    * correciones
    
    * .
    
    * .
    
    * .
    
    * .
    
    * correciones
    
    * fix
    
    * correciones
    
    ---------
    
    Co-authored-by: Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>

--- controllers/chapters/destroy.js
+++ controllers/chapters/destroy.js
@@ -3,7 +3,7 @@ import Chapter from "../../models/Chapter.js";
 const controller = {
   destroy: async (req, res, next) => {
     try {
-      let dst = await Chapter.deleteOne({_id: req.body._id});
+      let dst = await Chapter.deleteOne({_id: req.params.id});
       console.log(dst);
       return res.status(200).send(dst);
     } catch (error) {
