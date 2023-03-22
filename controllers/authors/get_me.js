8e3496154e7725bd3b5265fb173db3c4c62ac385
RominaGaite <gaiteromina@gmaul.com>
2023-03-21 22:21:59 -0300

    correcciones

--- controllers/authors/get_me.js
+++ controllers/authors/get_me.js
@@ -5,7 +5,8 @@ const controller = {
     get_me: async (req, res) => {
         try {
             let me = await Author.findOne({ user_id:req.user })//aca busca el usuario que esta registrado
-                .select("name last_name city country date photo ")
+                .select("name city country date photo ")
+               
             if (me) {
                 return res.status(200).json({
                     success: true,
--- controllers/authors/update.js
+++ controllers/authors/update.js
@@ -5,7 +5,8 @@ const controller = {
     update: async(req,res,next) => {
         try{
             let author = await Author.findOneAndUpdate({ user_id: req.user }, req.body, { new: true })
-            .select("name last_name city country date photo")
+            .select("name city country date photo ")
+           
             if(author){
                 return res.status(200).json({
                     success: true,
@@ -21,5 +22,6 @@ const controller = {
         }
     }
 }
+
 
 export default controller
