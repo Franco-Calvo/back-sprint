51237c7398503d5c408592452b3a154608f34a49
RominaGaite <gaiteromina@gmaul.com>
2023-03-28 16:56:37 -0300

    .

--- controllers/authors/get_me.js
+++ controllers/authors/get_me.js
@@ -4,11 +4,7 @@ import createError from "http-errors"
 const controller = {//inf prfl auth ok
     get_me: async (req, res) => {
         try {
-<<<<<<< HEAD
-            let me = await Author.findOne({ user_id:req.user })//aca busca el usuario que esta registrado
-=======
             let me = await Author.findOne({ user_id:req.user })
->>>>>>> af0580ae9ac8b0f1f14421b2f6123465170f9d43
                 .select("name city country date photo ")
                
             if (me) {
