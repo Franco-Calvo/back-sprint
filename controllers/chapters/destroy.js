5edd41a5df40ebb629d3694e3855b44db6393c43
DanielOQ <danielorozco0208@gmail.com>
2023-03-28 14:51:45 -0500

    fix 2

--- controllers/chapters/destroy.js
+++ controllers/chapters/destroy.js
@@ -3,11 +3,7 @@ import Chapter from "../../models/Chapter.js";
 const controller = {
   destroy: async (req, res, next) => {
     try {
-<<<<<<< HEAD
-      let dst = await Chapter.deleteOne({_id: req.body._id});
-=======
       let dst = await Chapter.deleteOne({_id: req.params.id});
->>>>>>> d61d0c9dac88ca38ab9c9e764d1514b2fdd05431
       console.log(dst);
       return res.status(200).send(dst);
     } catch (error) {
